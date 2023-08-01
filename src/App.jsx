0dee1ee283d4a90b86a6eedae947e579ec08f9d4
persjess <jesse.persinger.hvac@gmail.com>
2023-07-31 21:48:16 -0400

    created routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AllPlayers from './Components/AllPlayers'
+import SinglePlayer from './Components/SinglePlayer'
+import { Route, Routes, Link } from 'react-router-dom'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <nav>
+          <Link to='/'>Home</Link>
+          <Link to='/singleplayer'>Single Player</Link>
+        </nav>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <Routes>
+          <Route path="/" element={<AllPlayers />} />
+          <Route path="/singleplayer" element={<SinglePlayer />} />
+        </Routes>
+      </div >
     </>
   )
 }
---
+++ src/Components/SinglePlayer.jsx
@@ -0,0 +1,9 @@
+
+
+
+
+export default function SinglePlayer() {
+    return (
+        <h1>Single Players</h1>
+    )
+}
